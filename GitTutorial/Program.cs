84e3e20911c66065eafb26e7109a7bb45b4eeda0
asadhasnain <asadhasnain@gmail.com>
2018-04-23 12:54:56 +0500

    Feature A

--- GitTutorial/Program.cs
+++ GitTutorial/Program.cs
@@ -10,7 +10,7 @@ namespace GitTutorial
         {
         static void Main(string[] args)
             {
-            Console.WriteLine ("Hello World");
+            Console.WriteLine ("Hello World - Feature A");
             }
         }
     }
